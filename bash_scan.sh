0372c1ade7b2d51d2db9410145cb764a629d3853
Cristiano Coutinho <coutinho.cristiano@gmail.com>
2019-02-22 14:05:30 -0300

    Update bash_scan.sh

--- bash_scan.sh
+++ bash_scan.sh
@@ -5,7 +5,7 @@
 for srv in server1 server2
   do
     ssh -q root@$srv 'echo -n "$(uname -n) ... ";\
-      timeout 1 bash -c "cat < /dev/null > /dev/tcp/correio.bancobmg.com.br/25" 2> /dev/null;\
+      timeout 1 bash -c "cat < /dev/null > /dev/tcp/correio/25" 2> /dev/null;\
       echo $?'
  done
  
